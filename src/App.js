ce70b68ffe87c9f1bdb21af03efc3e58e6ffbfe5
bamboongyuen <bamboongyuen@gmail.com>
2023-01-16 09:53:26 +0700

    create project and base

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
 function App() {
-    const [content, setContent] = useState(false);
-    const handleClick = () => {
-        setContent(!content);
-    };
-    const ref = useRef();
-
-    useEffect(() => {});
-
     return (
         <div className="App">
             <h1>tiktok</h1>
-            <header className="App-header">
-                <button onClick={handleClick}>toggle</button>
-                {content && <Video />}
-            </header>
         </div>
     );
 }
